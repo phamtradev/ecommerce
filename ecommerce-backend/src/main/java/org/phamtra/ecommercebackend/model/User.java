e0681e98c2d15d17a14f0fda5c2c29f638065257
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-07-27 17:48:20 +0700

    first commit

---
+++ ecommerce-backend/src/main/java/org/phamtra/ecommercebackend/model/User.java
@@ -0,0 +1,34 @@
+package org.phamtra.ecommercebackend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 1000)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 320)
+    private String email;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+
+}
